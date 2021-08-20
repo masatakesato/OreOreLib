63439155fee851d4e22d50d512265b4d33b52899
masatake1115 <masatake1115@hotmail.co.jp>
2021-08-20 12:00:10 +0900

    Added DLLExport.h

---
+++ oreore/common/DLLExport.h
@@ -0,0 +1,15 @@
+﻿#ifndef DLL_EXPORT_H
+#define	DLL_EXPORT_H
+
+
+#ifdef EXPORTING_DLL
+	#define CLASS_DECLSPEC __declspec( dllexport )
+#else
+	#define CLASS_DECLSPEC __declspec( dllimport )
+#endif
+
+// Disable C4251 warnings
+#pragma warning(disable:4251)
+
+
+#endif // !DLL_EXPORT_H
