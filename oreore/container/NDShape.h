927d4568e8baabdb322bac93b48cce807908f00e
masatake1115 <masatake1115@hotmail.co.jp>
2021-09-21 11:25:42 +0900

    NDShape  - Performance test

--- oreore/container/NDShape.h
+++ oreore/container/NDShape.h
@@ -8,6 +8,25 @@
 
 
 
+//###############################################################################################################//
+//
+//  C-style array and initializer_list performance comparison using NDShape::ToND and NDShape::To1D method.
+//
+//	+-------------------+------------+-------------+
+//	|					|    read    |    write    |
+//	+-------------------+------------+-------------+
+//	|   C-style array	|    slower  |    faster   |
+//	+-------------------+------------+-------------+
+//	| initializer_list  |    faster  |    slower   |
+//	+-------------------+------------+-------------+
+//
+//   2021.09.21. VS2017(v141)
+//
+//###############################################################################################################//
+
+
+
+
 namespace OreOreLib
 {
 
@@ -83,6 +102,81 @@ namespace OreOreLib
 
 
 
+		//============== ND to 1D index conversion ===============//
+		// indexND[0] +				// x
+		// indexND[1] * m_Strides[0] +	// y
+		// indexND[2] * m_Strides[1] +	// z
+		// indexND[3] * m_Strides[2];	// w
+		// ...
+
+		template < typename ... Args >// initializer_list version
+		std::enable_if_t< (sizeof...(Args)==N) && TypeTraits::all_convertible<uint64, Args...>::value, int64 >
+		To1D( const Args& ... args ) const// x, y, z, w...
+		{
+			auto indexND = { args... };
+			auto itr = std::begin( indexND );
+
+			uint64 index = *(itr++);
+			auto offset = m_Strides;
+
+			while( itr !=std::end( indexND ) )
+				index += *(itr++) * *(offset++);
+
+			return index;
+		}
+		//template < typename ... Args >// C-style array version. slower than above implementation. 
+		//std::enable_if_t< (sizeof...(Args)==N) && TypeTraits::all_convertible<uint64, Args...>::value, int64 >
+		//To1D( const Args& ... args ) const// x, y, z, w...
+		//{
+		//	using T = typename TypeTraits::first_type<Args...>::type;
+		//	const T* indexND[N] = { &args... };
+		//	
+		//	int64 index = (T)*indexND[0];
+		//	for( int i=1; i<N; ++i )	index += *indexND[i] * m_Strides[i-1];
+		//	return index;
+		//}
+
+
+		template < typename T >
+		std::enable_if_t< std::is_convertible<uint64, T>::value, int64 >
+		To1D( std::initializer_list<T> indexND ) const// x, y, z, w...
+		{
+			auto itr = std::begin( indexND );
+
+			uint64 index = (uint64)*(itr++);
+			auto offset = m_Strides;
+
+			while( itr !=std::end( indexND ) )
+				index += (uint64)*(itr++) * *(offset++);
+
+			return index;
+		}
+
+
+		template < typename T >
+		std::enable_if_t< std::is_convertible_v<T, uint64>, int64 >
+		To1D( const T indexND[] ) const
+		{
+			int64 index = indexND[0];
+			for( int i=1; i<N; ++i )	index += indexND[i] * m_Strides[i-1];
+			return index;
+
+			//indexND[0] +					// x
+			//indexND[1] * m_Strides[0] +	// y
+			//indexND[2] * m_Strides[1] +	// z
+			//indexND[3] * m_Strides[2];	// w
+		}
+
+
+		template < typename T >
+		std::enable_if_t< std::is_convertible_v<T, uint64>, int64 >
+		From3DTo1D( const T& x, const T& y, const T& z ) const
+		{
+			return z * m_Strides[1] + y * m_Strides[0] + x;
+		}
+
+
+
 		//=============== 1D to ND index conversion ===============//
 		// indexND[3]	= ( id / m_Strides[2] );
 		// indexND[2]	= ( id % m_Strides[2] ) / m_Strides[1];
@@ -103,7 +197,6 @@ namespace OreOreLib
 		}
 
 
-
 		//　Single dim only
 		uint64 ToND( uint64 indexd1D, int dim ) const
 		{
@@ -118,107 +211,42 @@ namespace OreOreLib
 		}
 
 
-
-		template < typename ... Args >
+		template < typename ... Args >// C-style array version
 		std::enable_if_t< (sizeof...(Args)==N) && TypeTraits::all_convertible<uint64, Args...>::value, void >
 		ToND( uint64 index1D, Args& ... args ) const
 		{
 			using T = typename TypeTraits::first_type<Args...>::type;
 
-			auto indexND = { &args... };
-			auto head = indexND.begin();
-			auto iter =(T**)indexND.end();
-			iter--;
-			**iter = (T)index1D;
-			iter--;
-			
-			// (1)
-			for( ; iter>=indexND.begin(); --iter )
-			{
-				**iter = (T)123;
-				//tcout << *iter << tendl;
-				//indexND[i] = indexND[i+1] % (T)m_Strides[i];
-			}
-			
+			T* indexND[N] = { &args... };
 
-/*
-			(1)
-			indexND[N-1] = (T)indexd1D;
+			*indexND[N-1] = (T)index1D;
 			for( int i=N-2; i>=0; --i )
-				indexND[i] = indexND[i+1] % (T)m_Strides[i];
-
-			(2)
+				*indexND[i] = *indexND[i+1] % (T)m_Strides[i];
+			
 			for( int i=N-1; i>=1; --i )
-				indexND[i] /= (T)m_Strides[i-1];
-*/
+				*indexND[i] /= (T)m_Strides[i-1];
 		}
+		//template < typename ... Args >// initializer_list version. slower than above implementation. 
+		//std::enable_if_t< (sizeof...(Args)==N) && TypeTraits::all_convertible<uint64, Args...>::value, void >
+		//ToND( uint64 index1D, Args& ... args ) const
+		//{
+		//	using T = typename TypeTraits::first_type<Args...>::type;
 
+		//	auto indexND = { &args... };
 
+		//	auto iter_i = (T**)indexND.end();	iter_i--;
+		//	**iter_i = (T)index1D;//indexND[N-1] = (T)indexd1D;
 
-
-		//============== ND to 1D index conversion ===============//
-		// indexND[0] +				// x
-		// indexND[1] * m_Strides[0] +	// y
-		// indexND[2] * m_Strides[1] +	// z
-		// indexND[3] * m_Strides[2];	// w
-		// ...
-
-		template < typename ... Args >
-		std::enable_if_t< (sizeof...(Args)==N) && TypeTraits::all_convertible<uint64, Args...>::value, int64 >
-		To1D( const Args ... args ) const// x, y, z, w...
-		{
-			auto indexND = { args... };
-			auto itr = std::begin( indexND );
-
-			uint64 index = *(itr++);
-			auto offset = m_Strides;
-
-			while( itr !=std::end( indexND ) )
-				index += *(itr++) * *(offset++);
-
-			return index;
-		}
-
-
-		template < typename T >
-		std::enable_if_t< std::is_convertible<uint64, T>::value, int64 >
-		To1D( std::initializer_list<T> indexND ) const// x, y, z, w...
-		{
-			auto itr = std::begin( indexND );
-
-			uint64 index = (uint64)*(itr++);
-			auto offset = m_Strides;
-
-			while( itr !=std::end( indexND ) )
-				index += (uint64)*(itr++) * *(offset++);
-
-			return index;
-		}
-
-
-
-		template < typename T >
-		std::enable_if_t< std::is_convertible_v<T, uint64>, int64 >
-		To1D( const T (&indexND)[N] ) const
-		{
-			int64 index = indexND[0];
-			for( int i=1; i<N; ++i )	index += indexND[i] * m_Strides[i-1];
-			return index;
-
-			//indexND[0] +					// x
-			//indexND[1] * m_Strides[0] +	// y
-			//indexND[2] * m_Strides[1] +	// z
-			//indexND[3] * m_Strides[2];	// w
-		}
-
-
-		template < typename T >
-		std::enable_if_t< std::is_convertible_v<T, uint64>, int64 >
-		From3DTo1D( const T& x, const T& y, const T& z ) const
-		{
-			return z * m_Strides[1] + y * m_Strides[0] + x;
-		}
-
+		//	auto iter_i_1 = iter_i;
+		//	auto iter_stride = &m_Strides[N-2];
+		//	while( --iter_i >= indexND.begin() )
+		//		**iter_i = **iter_i_1-- % *iter_stride--;
+		//	
+		//	iter_i = (T**)indexND.end();
+		//	iter_stride = &m_Strides[N-2];
+		//	while( --iter_i > indexND.begin() )
+		//		**iter_i /= (T)*iter_stride--;
+		//}
 
 
 
--- test/testNDShape/testNDShape_main.cpp
+++ test/testNDShape/testNDShape_main.cpp
@@ -6,7 +6,7 @@ using namespace OreOreLib;
 
 
 
-const int c_LoopCount = 1;//1000000;//0;
+const int c_LoopCount = 1;//10000000;//0;
 
 int main()
 {
@@ -21,12 +21,12 @@ int main()
 
 
 	{
-		tcout << "array...\n";
+		tcout << "NDShape::To1D( const T (&indexND)[N] ) const...\n";
 
 		std::chrono::system_clock::time_point  start, end; // 型は auto で可
 		start = std::chrono::system_clock::now(); // 計測開始時間
 
-		for( int i=0; i<c_LoopCount; ++i)
+		for( int k=0; k<c_LoopCount; ++k)
 		for( int z=0; z<idx.Dim(2); ++z )
 		{
 			for( int y=0; y<idx.Dim( 1 ); ++y )
@@ -34,7 +34,7 @@ int main()
 				for( int x=0; x<idx.Dim(0); ++x )
 				{
 					int index[3] = {x, y, z };
-					auto idx1d = idx.To1D( index );//idx.To1D( x, y, z );//
+					auto idx1d = idx.To1D( index );
 					tcout << "[" << z << "][" << y << "][" << x << "] -> [" << idx1d << "]\n";
 				}
 			}
@@ -49,20 +49,19 @@ int main()
 	tcout << tendl;
 
 	{
-		tcout << "variadic templates...\n";
+		tcout << "NDArray::To1D( const Args& ... args ) const...\n";
 
 		std::chrono::system_clock::time_point  start, end; // 型は auto で可
 		start = std::chrono::system_clock::now(); // 計測開始時間
 
-		for( int i=0; i<c_LoopCount; ++i)
+		for( int k=0; k<c_LoopCount; ++k)
 		for( int z=0; z<idx.Dim(2); ++z )
 		{
 			for( int y=0; y<idx.Dim(1); ++y )
 			{
 				for( int x=0; x<idx.Dim(0); ++x )
 				{
-					//int index[3] = {x, y, z };
-					auto idx1d = idx.To1D( x, y, z );//idx.To1D( index );//
+					auto idx1d = idx.To1D( x, y, z );
 					tcout << "[" << z << "][" << y << "][" << x << "] -> (" << idx1d << ")\n";
 				}
 			}
@@ -77,12 +76,12 @@ int main()
 	tcout << tendl;
 
 	{
-		tcout << "initializer list...\n";
+		tcout << "NDArray::To1D( std::initializer_list<T> indexND ) const...\n";
 
 		std::chrono::system_clock::time_point  start, end; // 型は auto で可
 		start = std::chrono::system_clock::now(); // 計測開始時間
 
-		for( int i=0; i<c_LoopCount; ++i)
+		for( int k=0; k<c_LoopCount; ++k)
 		for( int z=0; z<idx.Dim(2); ++z )
 		{
 			for( int y=0; y<idx.Dim( 1 ); ++y )
@@ -104,19 +103,19 @@ int main()
 	tcout << tendl;
 
 	{
-		tcout << "fixed arguments...\n";
+		tcout << "NDArray::From3DTo1D( const T& x, const T& y, const T& z ) const...\n";
 
 		std::chrono::system_clock::time_point  start, end; // 型は auto で可
 		start = std::chrono::system_clock::now(); // 計測開始時間
 
-		for( int i=0; i<c_LoopCount; ++i)
+		for( int k=0; k<c_LoopCount; ++k)
 		for( int z=0; z<idx.Dim(2); ++z )
 		{
 			for( int y=0; y<idx.Dim( 1 ); ++y )
 			{
 				for( int x=0; x<idx.Dim(0); ++x )
 				{
-					auto idx1d = idx.From3DTo1D( x, y, z );//idx.To1D( index );//
+					auto idx1d = idx.From3DTo1D( x, y, z );
 					tcout << "[" << z << "][" << y << "][" << x << "] -> (" << idx1d << ")\n";
 				}
 			}
@@ -130,22 +129,48 @@ int main()
 
 	tcout << tendl;
 
-
-
-
-	for(int i=0; i<27; ++i )
 	{
-		int idx_[3];
-		idx.ToND( i, idx_ );
+		tcout << "NDArray::ToND( uint64 indexd1D, T indexND[] ) const...\n";
 
-		tcout << "[" << i << "] -> [" << idx_[2] << "][" << idx_[1] << "][" << idx_[0] << "]\n";
+		std::chrono::system_clock::time_point  start, end; // 型は auto で可
+		start = std::chrono::system_clock::now(); // 計測開始時間
+
+		for( int k=0; k<c_LoopCount; ++k)
+		for(int i=0; i<27; ++i )
+		{
+			int idx_[3];
+			idx.ToND( i, idx_ );
+			tcout << "[" << i << "] -> [" << idx_[2] << "][" << idx_[1] << "][" << idx_[0] << "]\n";
+		}
+
+		end = std::chrono::system_clock::now();  // 計測終了時間
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( end-start ).count(); //処理に要した時間をミリ秒に変換
+
+		tcout << "time elapsed: " << elapsed << "[ms].\n";
 	}
 
+	tcout << tendl;
 
-	int x, y, z;
-	idx.ToND( 0, x, y, z );
+	{
+		tcout << "NDArray::ToND( uint64 index1D, Args& ... args ) const...\n";
+		
+		std::chrono::system_clock::time_point  start, end; // 型は auto で可
+		start = std::chrono::system_clock::now(); // 計測開始時間
 
-	tcout << x << ", " << y << ", " << z << tendl;
+		for( int k=0; k<c_LoopCount; ++k)
+		for(int i=0; i<27; ++i )
+		{
+			int x, y, z;
+			idx.ToND( i, x, y, z );
+			tcout << "[" << i << "] -> [" << z << "][" << y << "][" << x << "]\n";
+		}
+
+		end = std::chrono::system_clock::now();  // 計測終了時間
+		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( end-start ).count(); //処理に要した時間をミリ秒に変換
+
+		tcout << "time elapsed: " << elapsed << "[ms].\n";
+	}
+
 
 	return 0;
 }
