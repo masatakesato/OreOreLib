0eeb37d28c6c647e5b5b5eae6a1107c05595da4a
masatake1115 <masatake1115@hotmail.co.jp>
2021-09-28 16:47:18 +0900

    Exit loop test

---
+++ test/testLoopExitonKeyPress/testLoopExitonKeyPress_main.cpp
@@ -0,0 +1,51 @@
+﻿// https://hotnews8.net/programming/tricky-code/c-code02
+
+#include <windows.h>
+#include <conio.h>
+
+#include	<oreore/common/TString.h>
+
+
+
+
+#define ESC 27
+
+
+
+
+void func()
+{
+	int val=0;
+	for( int i=0; i<100000; ++i )
+	{
+		for( int j=0; j<10000; ++j )
+			val++;
+
+		for( int j=0; j<10000; ++j )
+			val--;
+	}
+}
+
+
+
+int main(void)
+{
+    int cnt = 0;
+
+    while (1) 
+	{
+		func();
+
+
+        if (_kbhit() && _getch() == ESC)
+		{
+			tcout << "You Hit Esc-Key!!\n";
+			//_cputs("\r\nYou Hit Esc-Key!!\r\n");
+            break;
+        }
+
+        tcout << ++cnt << tendl;
+    }
+
+    return 0;
+}
