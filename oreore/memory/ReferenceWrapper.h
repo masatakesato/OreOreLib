b08c40319a920ecb503540626b4a5c8120dc7b44
masatake1115 <masatake1115@hotmail.co.jp>
2021-11-06 23:57:38 +0900

    Added ReferenceWrapper

---
+++ oreore/memory/ReferenceWrapper.h
@@ -0,0 +1,105 @@
+﻿#ifndef REFERENCE_WRAPPER_H
+#define	REFERENCE_WRAPPER_H
+
+
+namespace OreOreLib
+{
+	template < typename T >
+	class ReferenceWrapper
+	{
+	public:
+
+		// Default constructor
+		ReferenceWrapper()
+			: m_Ptr( nullptr )
+		{
+		}
+
+
+		// Constructor
+		ReferenceWrapper( T& ref )
+			: m_Ptr( &ref )
+		{
+		}
+
+
+		// Copy constructor
+		ReferenceWrapper( const ReferenceWrapper& obj )
+			: m_Ptr( obj.m_Ptr )
+		{
+		}
+
+
+		// Move constructor
+		ReferenceWrapper( ReferenceWrapper&& obj )
+			: m_Ptr( &obj.m_Ptr )
+		{
+			obj.m_Ptr = nullptr;
+		}
+
+
+		// Destructor
+		~ReferenceWrapper()
+		{
+			m_Ptr = nullptr;
+		}
+
+
+		// Copy assingment operator
+		ReferenceWrapper& operator=( const ReferenceWrapper& obj )
+		{
+			if( m_Ptr != obj.m_Ptr )
+				m_Ptr = obj.m_Ptr;
+
+			return *this;
+		}
+
+
+		// Move assingment operator
+		ReferenceWrapper& operator=( ReferenceWrapper&& obj )
+		{
+			if( m_Ptr != obj.m_Ptr )
+			{
+				m_Ptr = obj.m_Ptr;
+				obj.m_Ptr = nullptr;
+			}
+
+			return *this;
+		}
+
+
+		bool operator==( const ReferenceWrapper& obj ) const
+		{
+			return m_Ptr == obj.m_Ptr;
+		}
+
+
+		operator T&() const
+		{
+			return *m_Ptr;
+		}
+
+
+		T& Ref() const
+		{
+			return *m_Ptr;
+		}
+
+
+		T* Ptr() const
+		{
+			return m_Ptr;
+		}
+
+
+
+	private:
+
+		T*	m_Ptr;
+	};
+
+
+}// end of namespace OreOreLib
+
+
+#endif // !REFERENCE_WRAPPER_H
---
+++ test/testReferenceWrapper/testReferenceWrapper_main.cpp
@@ -0,0 +1,25 @@
+﻿#include	<oreore/common/TString.h>
+#include	<oreore/memory/ReferenceWrapper.h>
+using namespace OreOreLib;
+
+
+int main()
+{
+	int a=66;
+	int b= -55555;
+	ReferenceWrapper<int> ref( a );
+
+	tcout << ref << tendl;
+
+	ref.Ref() = 999;
+
+	tcout << ref << tendl;
+
+
+//	ref.Ref() = b;
+	ref = b;
+
+	tcout << ref << tendl;
+
+	return 0;
+}
