4a2683cac8e01d8795c216b2823d6ce06dee5b4c
masatake1115 <masatake1115@hotmail.co.jp>
2021-12-19 13:02:27 +0900

    Implementing Timer class

---
+++ oreore/time/Timer.h
@@ -0,0 +1,76 @@
+﻿#ifndef TIMER_H
+#define	TIMER_H
+
+#include	<Windows.h>
+
+#include	"../common/Utility.h"
+
+
+
+namespace OreOreLib
+{
+
+	//template < typename T >
+	class Timer
+	{
+	public:
+
+
+		void Init()
+		{
+			m_Start = (float)timeGetTime();
+			m_End = m_Start;
+		}
+
+
+		void SetSleepTime( const float& val )
+		{
+			m_SleepTime = val;
+		}
+
+
+		void Start()
+		{
+			m_Start = (float)timeGetTime();
+		}
+
+
+		void End()
+		{
+			m_End = (float)timeGetTime();
+
+			auto timeElapsed = m_End - m_Start;
+			auto wait = m_SleepTime - timeElapsed;
+			if( wait > 0 )
+			{
+				Sleep( wait );
+				timeElapsed += wait;
+			}
+
+			m_DetlaTime	= timeElapsed;
+			//m_DetlaTime = 0.5f * ( m_DetlaTime + (timeElapsed) );// moving average ver
+		}
+
+
+		float DeltaTime()
+		{
+			return m_DetlaTime;
+		}
+
+
+
+	private:
+
+		float	m_DetlaTime;
+
+		float	m_Start;
+		float	m_End;
+
+		float	m_SleepTime;
+	};
+
+
+}// end of namespace OreOreLib
+
+
+#endif // !TIMER_H
