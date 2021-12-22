cb4bf419adadbd158ed73625d0c1df4c76823e4a
masatake1115 <masatake1115@hotmail.co.jp>
2021-12-22 12:15:51 +0900

    Refactored FreeImageLoader(sRGB support)

--- oreore/images/FreeImageLoader.h
+++ oreore/images/FreeImageLoader.h
@@ -75,52 +75,52 @@ namespace OreOreLib
 	{
 		switch( data_format )
 		{
-		case DATA_FORMAT::FORMAT_UNKNOWN:// unknown type
+		case FORMAT_UNKNOWN:// unknown type
 			return FIT_UNKNOWN;
 
-		case DATA_FORMAT::FORMAT_R8G8B8_UNORM:// standard image			: 1-, 4-, 8-, 16-, 24-, 32-bit
+		case FORMAT_R8G8B8_UNORM:// standard image			: 1-, 4-, 8-, 16-, 24-, 32-bit
 			return FIT_BITMAP;
 
-		case DATA_FORMAT::FORMAT_B8G8R8_UNORM:
+		case FORMAT_B8G8R8_UNORM:
 			return FIT_BITMAP;
 
-		case DATA_FORMAT::FORMAT_R16_UNORM:// array of unsigned short	: unsigned 16-bit
+		case FORMAT_R16_UNORM:// array of unsigned short	: unsigned 16-bit
 			return FIT_UINT16;
 
-		case DATA_FORMAT::FORMAT_R16_SINT:// array of short			: signed 16-bit
+		case FORMAT_R16_SINT:// array of short			: signed 16-bit
 			return FIT_INT16;
 
-		case DATA_FORMAT::FORMAT_R32_UINT:// array of unsigned long	: unsigned 32-bit
+		case FORMAT_R32_UINT:// array of unsigned long	: unsigned 32-bit
 			return FIT_UINT32;
 
-		case DATA_FORMAT::FORMAT_R32_SINT:// array of long			: signed 32-bit
+		case FORMAT_R32_SINT:// array of long			: signed 32-bit
 			return FIT_INT32;
 
-		case DATA_FORMAT::FORMAT_R32_FLOAT:// array of float			: 32-bit IEEE floating point
+		case FORMAT_R32_FLOAT:// array of float			: 32-bit IEEE floating point
 			return FIT_FLOAT;
 
-			//case DATA_FORMAT::FORMAT_UNKNOWN:// array of double			: 64-bit IEEE floating point
+			//case FORMAT_UNKNOWN:// array of double			: 64-bit IEEE floating point
 			//	return FIT_DOUBLE;
 
-			//case DATA_FORMAT::FORMAT_UNKNOWN:// array of FICOMPLEX		: 2 x 64-bit IEEE floating point
+			//case FORMAT_UNKNOWN:// array of FICOMPLEX		: 2 x 64-bit IEEE floating point
 			//	return FIT_COMPLEX;
 
-		case DATA_FORMAT::FORMAT_R16G16B16_UNORM:// 48-bit RGB image			: 3 x 16-bit
+		case FORMAT_R16G16B16_UNORM:// 48-bit RGB image			: 3 x 16-bit
 			return FIT_RGB16;
 
-		case DATA_FORMAT::FORMAT_R16G16B16_FLOAT:// 48-bit RGB float image			: 3 x 16-bit
+		case FORMAT_R16G16B16_FLOAT:// 48-bit RGB float image			: 3 x 16-bit
 			return FIT_RGB16;
 
-		case DATA_FORMAT::FORMAT_R16G16B16A16_UNORM:// 64-bit RGBA image		: 4 x 16-bit
+		case FORMAT_R16G16B16A16_UNORM:// 64-bit RGBA image		: 4 x 16-bit
 			return FIT_RGBA16;
 
-		case DATA_FORMAT::FORMAT_R16G16B16A16_FLOAT:// 64-bit RGBA float image		: 4 x 16-bit
+		case FORMAT_R16G16B16A16_FLOAT:// 64-bit RGBA float image		: 4 x 16-bit
 			return FIT_RGBA16;
 
-		case DATA_FORMAT::FORMAT_R32G32B32_FLOAT:// 96-bit RGB float image	: 3 x 32-bit IEEE floating point
+		case FORMAT_R32G32B32_FLOAT:// 96-bit RGB float image	: 3 x 32-bit IEEE floating point
 			return FIT_RGBF;
 
-		case DATA_FORMAT::FORMAT_R32G32B32A32_FLOAT:// 128-bit RGBA float image	: 4 x 32-bit IEEE floating point
+		case FORMAT_R32G32B32A32_FLOAT:// 128-bit RGBA float image	: 4 x 32-bit IEEE floating point
 			return FIT_RGBAF;
 
 		default:// unknown type
@@ -132,56 +132,59 @@ namespace OreOreLib
 
 
 
-	static DATA_FORMAT GetDataFormat( FREE_IMAGE_TYPE fit, int bpp )
+	static DATA_FORMAT GetDataFormat( FIBITMAP* dib, bool bSRGB=false )
 	{
+		auto fit = FreeImage_GetImageType( dib );
+		auto bpp = FreeImage_GetBPP( dib );
+
 		switch( fit )
 		{
 		case FIT_UNKNOWN:// unknown type
-			return DATA_FORMAT::FORMAT_UNKNOWN;
+			return FORMAT_UNKNOWN;
 
 		case FIT_BITMAP:// standard image			: 1-, 4-, 8-, 16-, 24-, 32-bit
-			if( bpp == 8 )		return DATA_FORMAT::FORMAT_R8_UNORM;
-			//else if(bpp == 16)		return DATA_FORMAT::FORMAT_G8R8_UNORM;
-			else if( bpp==24 )			return DATA_FORMAT::FORMAT_B8G8R8_UNORM;
-			else if( bpp == 32 )	return DATA_FORMAT::FORMAT_B8G8R8A8_UNORM;
+			if( bpp == 8 )		return FORMAT_R8_UNORM;
+			//else if(bpp==16)	return FORMAT_G8R8_UNORM;
+			else if( bpp==24 )	return bSRGB ? FORMAT_B8G8R8_SRGB : FORMAT_B8G8R8_UNORM;
+			else if( bpp==32 )	return bSRGB ? FORMAT_B8G8R8A8_UNORM_SRGB : FORMAT_B8G8R8A8_UNORM;
 
-			return DATA_FORMAT::FORMAT_B8G8R8A8_UNORM;//return DATA_FORMAT::FORMAT_R8G8B8A8_UNORM;
+			return bSRGB ? FORMAT_B8G8R8A8_UNORM_SRGB : FORMAT_B8G8R8A8_UNORM;//return FORMAT_R8G8B8A8_UNORM;
 
 		case FIT_UINT16:// array of unsigned short	: unsigned 16-bit
-			return DATA_FORMAT::FORMAT_R16_UNORM;
+			return FORMAT_R16_UNORM;
 
 		case FIT_INT16:// array of short			: signed 16-bit
-			return DATA_FORMAT::FORMAT_R16_SINT;
+			return FORMAT_R16_SINT;
 
 		case FIT_UINT32:// array of unsigned long	: unsigned 32-bit
-			return DATA_FORMAT::FORMAT_R32_UINT;
+			return FORMAT_R32_UINT;
 
 		case FIT_INT32:// array of long			: signed 32-bit
-			return DATA_FORMAT::FORMAT_R32_SINT;
+			return FORMAT_R32_SINT;
 
 		case FIT_FLOAT:// array of float			: 32-bit IEEE floating point
-			return DATA_FORMAT::FORMAT_R32_FLOAT;
+			return FORMAT_R32_FLOAT;
 
 		case FIT_DOUBLE:// array of double			: 64-bit IEEE floating point
-			return DATA_FORMAT::FORMAT_UNKNOWN;
+			return FORMAT_UNKNOWN;
 
 		case FIT_COMPLEX:// array of FICOMPLEX		: 2 x 64-bit IEEE floating point
-			return DATA_FORMAT::FORMAT_UNKNOWN;
+			return FORMAT_UNKNOWN;
 
 		case FIT_RGB16:// 48-bit RGB image			: 3 x 16-bit
-			return DATA_FORMAT::FORMAT_R16G16B16_UNORM;
+			return FORMAT_R16G16B16_UNORM;
 
 		case FIT_RGBA16:// 64-bit RGBA image		: 4 x 16-bit
-			return DATA_FORMAT::FORMAT_R16G16B16A16_UNORM;
+			return FORMAT_R16G16B16A16_UNORM;
 
 		case FIT_RGBF:// 96-bit RGB float image	: 3 x 32-bit IEEE floating point
-			return DATA_FORMAT::FORMAT_R32G32B32_FLOAT;
+			return FORMAT_R32G32B32_FLOAT;
 
 		case FIT_RGBAF:// 128-bit RGBA float image	: 4 x 32-bit IEEE floating point
-			return DATA_FORMAT::FORMAT_R32G32B32A32_FLOAT;
+			return FORMAT_R32G32B32A32_FLOAT;
 
 		default:// unknown type
-			return DATA_FORMAT::FORMAT_UNKNOWN;
+			return FORMAT_UNKNOWN;
 
 		};
 	}
