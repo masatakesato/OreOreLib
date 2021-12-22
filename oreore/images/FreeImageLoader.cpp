88b76b89d50d03dd0c7187c79f1034e648dd4918
masatake1115 <masatake1115@hotmail.co.jp>
2021-12-22 19:07:49 +0900

    FreeImageLoader  -unicode/multibyte support

--- oreore/images/FreeImageLoader.cpp
+++ oreore/images/FreeImageLoader.cpp
@@ -24,16 +24,28 @@ namespace OreOreLib
 	FIBITMAP* FreeImageLoader::Load( const tstring& filepath, bool bFloat, bool bFlip )
 	{
 		// Check(or estimate) filetype
+#if defined(UNICODE) || defined(_UNICODE)
 		auto fif = FreeImage_GetFileTypeU( filepath.c_str() );
+#else
+		auto fif = FreeImage_GetFileType( filepath.c_str() );
+#endif
 
 		if( fif == FIF_UNKNOWN )
-			/*FreeImage_GetFIFFromFilename*/FreeImage_GetFIFFromFilenameU( filepath.c_str() );
+#if defined(UNICODE) || defined(_UNICODE)
+			FreeImage_GetFIFFromFilenameU( filepath.c_str() );
+#else
+			FreeImage_GetFIFFromFilename( filepath.c_str() );
+#endif
 
 		if( fif==FIF_UNKNOWN )
 			return nullptr;
 
 		// Load Image data
+#if defined(UNICODE) || defined(_UNICODE)
 		auto dib = FreeImage_LoadU( fif, filepath.c_str() );
+#else
+		auto dib = FreeImage_Load( fif, filepath.c_str() );
+#endif
 		if( !dib )
 			return nullptr;
 
@@ -47,21 +59,6 @@ namespace OreOreLib
 		else
 			dib = FreeImage_ConvertTo32Bits( dib );
 
-
-		auto width	= FreeImage_GetWidth( dib );
-		auto height	= FreeImage_GetHeight( dib );
-		auto depth	= FINumChannels( dib );
-
-		// Create tensor from raw pixel array data
-		auto rawdata = FreeImage_GetBits( dib );
-
-
-
-
-
-		// Uload Freeimage buffer
-		//FreeImage_Unload( dib );
-
 		return dib;
 	}
 
@@ -69,14 +66,18 @@ namespace OreOreLib
 
 	bool FreeImageLoader::Save( const tstring& filepath, const void* pData, uint32 width, uint32 height, uint32 pixelByteSize, bool bFlip )
 	{
-		//// Check filetype from extension
-		//FREE_IMAGE_FORMAT fif	= /*FreeImage_GetFIFFromFilename*/FreeImage_GetFIFFromFilenameU( filepath.c_str() );
+		// Check filetype from extension
+//#if defined(UNICODE) || defined(_UNICODE)
+//		FREE_IMAGE_FORMAT fif	= FreeImage_GetFIFFromFilenameU( filepath.c_str() );
+//#else
+//		FREE_IMAGE_FORMAT fif	= FreeImage_GetFIFFromFilename( filepath.c_str() );
+//#endif
 
-		//// フォーマット不明 or 未対応フォーマットの場合は処理を中止する
+		// フォーマット不明 or 未対応フォーマットの場合は処理を中止する
 		//if( fif == FIF_UNKNOWN || FreeImage_FIFSupportsReading( fif ) == FALSE )
 		//	return false;
 
-// TODO: Implement GetFreeImageType
+		// TODO: Implement GetFreeImageType
 		//FREE_IMAGE_TYPE fit	= GetFreeImageType( texDesc.DataFormat );
 		//auto bpp = pixelByteSize * 8;
 		//FIBITMAP* dib = FreeImage_AllocateT( fit, width, height, bpp );
@@ -86,7 +87,11 @@ namespace OreOreLib
 		//if( bFlip )
 		//	FreeImage_FlipVertical( dib );
 
+//#if defined(UNICODE) || defined(_UNICODE)
 		//FreeImage_SaveU( fif, dib, filepath );
+//#else
+		//FreeImage_Save( fif, dib, filepath );
+//#endif
 
 		return true;
 	}
