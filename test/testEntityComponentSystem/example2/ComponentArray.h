ac9b05c00db68e84fb230f49d2f5ae24678329cf
masatake1115 <masatake1115@hotmail.co.jp>
2021-12-28 12:18:33 +0900

    Implementing ECS

--- test/testEntityComponentSystem/example2/ComponentArray.h
+++ test/testEntityComponentSystem/example2/ComponentArray.h
@@ -25,37 +25,62 @@ public:
 
 	void InsertData( Entity entity, T component )
 	{
+		ASSERT( !mEntityToIndexMap.Exists(entity) && "Component already exists." );
 
+		size_t newIndex = mSize;
+		mEntityToIndexMap[ entity ] = newIndex;
+		mIndexToEntityMap[ newIndex ] = entity;
+		mComponentArray[ newIndex ] = component;
+		++mSize;
 	}
 
 
 	void RemoveData( Entity entity )
 	{
+		ASSERT( mEntityToIndexMap.Exists(entity) && "Component does not exist." );
 
+		// 削除予定位置の要素を最終要素で上書きする
+		size_t indexOfRemovedEntity = mEntityToIndexMap[ entity ];
+		size_t indexOfLastElement = mSize - 1;
+		mComponentArray[ indexOfRemovedEntity ] = mComponentArray[ indexOfLastElement ];
+
+		// 
+		Entity entityOfLastElement = mIndexToEntityMap[ indexOfLastElement ];
+		mEntityToIndexMap[ entityOfLastElement ] = indexOfRemovedEntity;// 最終要素Entity -> 空いた要素番号
+		mIndexToEntityMap[ indexOfRemovedEntity ] = entityOfLastElement;// 空いた要素番号 -> 最終要素Entity
+
+		mEntityToIndexMap.Remove( entity );
+		mIndexToEntityMap.Remove( indexOfLastElement );
+
+		--mSize;
 	}
 
 
 	T& GetData( Entity entity )
 	{
+		ASSERT( mEntityToIndexMap.Exists(entity) && "Component does not exist." );
 
+		return mComponentArray[ mEntityToIndexMap[entity] ];
 	}
 
 
 	void EntityDestroyed( Entity entity ) override
 	{
-
+		if( mEntityToIndexMap.Exists(entity) )
+			RemoveData( entity );
 	}
 
 
 
 private:
 
-
+	// コンポーネントを格納した配列. 隙間だらけになってる可能性あり
 	OreOreLib::StaticArray<T, MAX_ENTITIES>	mComponentArray;
 
+	// エンティティIDからコンポーネント配列mComponentArrayインデックスへの変換テーブル
 	OreOreLib::HashMap<Entity, size_t, 128>	mEntityToIndexMap;
 
-
+	// 通し番号で利用中の要素を検索するためのインデックステーブル
 	OreOreLib::HashMap<Entity, size_t, 128>	mIndexToEntityMap;
 
 	size_t	mSize;
---
+++ test/testEntityComponentSystem/example2/ComponentManager.h
@@ -0,0 +1,34 @@
+﻿#ifndef COMPONENT_MANAGER_H
+#define	COMPONENT_MANAGER_H
+
+#include	"ECSCommon.h"
+
+
+
+class ComponentManager
+{
+public:
+
+	template< typename T >
+	void RegisterComponent()
+	{
+
+	}
+
+
+	template < typename T >
+	ComponentType GetComponentType()
+	{
+
+	}
+
+
+
+private:
+
+};
+
+
+
+
+#endif // !COMPONENT_MANAGER_H
