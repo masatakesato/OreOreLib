b1d66dcc985e3595cdd828aadab96b0e33fbb58f
masatake1115 <masatake1115@hotmail.co.jp>
2022-03-14 22:07:25 +0900

    Transfer c array to python using numpy(wip)

--- test/testCallPython/py_test1.py
+++ test/testCallPython/py_test1.py
@@ -3,5 +3,28 @@ import random
 def func():
 	a = [ 111, 222, 333, 444, 555 ]
 	b = random.choice(a)
-	print("hogehoge")
+	print( "py_test1.func..." )
 	return b
+
+
+count = 0
+
+
+def func2( input ):
+	print( "py_test1.func2" )
+	a = input
+	print( a )
+	list_data = [ 3, 7, 9, 11 ]
+	list_data.append( a )
+	b = random.choice( list_data )
+
+	global count
+	count += 1
+	print( count )
+
+	return b
+
+
+
+def func3( array ):
+	print( "py_test1.func3" )
--- test/testCallPython/testCallPython_main.cpp
+++ test/testCallPython/testCallPython_main.cpp
@@ -3,10 +3,14 @@
 #ifdef _DEBUG
 #undef	_DEBUG
 #include	<Python.h>
+#include	<../Lib/site-packages/numpy/core/include/numpy/arrayobject.h>
 #define	_DEBUG
 #else
 #include	<Python.h>
+#include	<../Lib/site-packages/numpy/core/include/numpy/arrayobject.h>
 #endif // _DEBUG
+
+
 
 
 // https://gist.github.com/KobayashiRui/e2598d6cbee95897561f9a4df41d8033
@@ -17,9 +21,12 @@ int main()
 {
 	PyObject *pName, *pModule, *pTmp, *pFunc;
 	char *sTmp;
-	int data;// Variable to store output from python
+	int func_output;// Variable to store output from python
+	double func2_input = 555.5;
+	double func2_output;
 
 	Py_Initialize();
+	
 
 	// Set system paths
 	PyObject *sys = PyImport_ImportModule("sys");
@@ -27,19 +34,43 @@ int main()
 	PyList_Append( path, PyUnicode_DecodeFSDefault(".") );// current directory( *.vsproj directory)
 	PyList_Append( path, PyUnicode_DecodeFSDefault("../../../test/testCallPython") );// source file directory
 
+
 	pName = PyUnicode_DecodeFSDefault( "py_test1" );
 	pModule = PyImport_Import( pName );
 	Py_DECREF( pName );
 
 	if( pModule != NULL )
 	{
+		// call func
 		pFunc = PyObject_GetAttrString( pModule, "func" );
 
 		pTmp = PyObject_CallObject( pFunc, NULL );
+		PyArg_Parse( pTmp, "i", &func_output );
+		std::cout << func_output << std::endl;
 
-		PyArg_Parse( pTmp, "i", &data );
-		std::cout << data << std::endl;
+
+		// call func2
+		auto pFunc2 = PyObject_GetAttrString( pModule, "func2" );
+		auto pArgs = PyTuple_New(1);
+		auto pFloatValue = PyFloat_FromDouble( func2_input );
+		PyTuple_SetItem( pArgs, 0, pFloatValue );
+
+		pTmp = PyObject_CallObject( pFunc2, pArgs );
+		PyArg_Parse( pTmp, "d", &func2_output );
+		std::cout << func2_output << std::endl;
+
+
+		// call func3 https://stackoverflow.com/questions/30388170/sending-a-c-array-to-python-and-back-extending-c-with-numpy
+		auto pArgsArr = PyTuple_New(1);
+
+		npy_intp dims[1] = { 4 };
+		float Array [] = {1.2, 3.4, 5.6, 7.8};
+		float *ptr = Array;
+		import_array();
+		auto py_array = PyArray_SimpleNewFromData( 1, dims, NPY_FLOAT, ptr );
+
 	}
+
 	Py_Finalize();
 
 
