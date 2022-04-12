18396f8d9ae40cf49f121144cc019a9ec66437ab
masatake1115 <masatake1115@hotmail.co.jp>
2022-04-12 22:09:07 +0900

    testTypeToIndexConversion

---
+++ test/testTypeToIndexConversion/testTypeToIndexConversion_main.cpp
@@ -0,0 +1,53 @@
+﻿#include	<iostream>
+#include	<string>
+using namespace std;
+
+#include	<oreore/common/Utility.h>
+#include	<oreore/memory/DebugNew.h>
+
+
+
+
+template < typename T >
+struct TypeID
+{
+	static const uint32 value;
+};
+
+template < typename T >
+const uint32 TypeID<T>::value = detail::SeqID::Generate();
+
+
+namespace detail
+{
+	class SeqID
+	{
+	private:
+
+		static uint32 Generate()
+		{
+			static uint32 counter = 0;
+			return counter++;
+		}
+
+		template < typename T >
+		friend struct TypeID;
+	};
+
+}
+
+
+
+
+
+int main()
+{
+	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+
+	tcout << TypeID<int>::value << tendl;
+	tcout << TypeID<float>::value << tendl;
+	tcout << TypeID<double>::value << tendl;
+	tcout << TypeID<int>::value << tendl;
+
+	return 0;
+}
