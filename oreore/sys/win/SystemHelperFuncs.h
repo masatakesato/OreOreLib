d3eb31f2fee8d173b06526fddc1400981ba7bfe1
masatake1115 <masatake1115@hotmail.co.jp>
2022-07-22 21:39:16 +0900

    Added system helper functions

---
+++ oreore/sys/win/SystemHelperFuncs.h
@@ -0,0 +1,62 @@
+﻿#ifndef WIN_HELPER_FUNCTIONS_H
+#define	WIN_HELPER_FUNCTIONS_H
+
+#include	<Windows.h>
+
+
+
+namespace OreOreLib
+{
+
+	static BOOL CALLBACK SendWMCloseMsg( HWND hwnd, LPARAM lParam )
+	{
+		DWORD dwProcessId = 0;
+		GetWindowThreadProcessId( hwnd, &dwProcessId );
+
+		if( dwProcessId == lParam )
+			SendMessageTimeout( hwnd, WM_CLOSE, 0, 0, SMTO_ABORTIFHUNG, 30000, NULL );
+
+		return TRUE;
+	}
+
+
+
+	static void CloseProcess( PROCESS_INFORMATION pInfo, DWORD timeout_interval=0 )
+	{
+		CloseHandle( pInfo.hThread );
+		WaitForInputIdle( pInfo.hProcess, INFINITE );
+
+		if( WaitForSingleObject( pInfo.hProcess, timeout_interval ) == WAIT_TIMEOUT )
+		{
+			EnumWindows( &SendWMCloseMsg, pInfo.dwProcessId );
+			// force termination if app cannot be closed.
+			if( WaitForSingleObject( pInfo.hProcess, timeout_interval ) == WAIT_TIMEOUT )
+				TerminateProcess( pInfo.hProcess, 0 );
+		}
+
+		CloseHandle( pInfo.hProcess );
+	}
+
+
+
+	static void CloseProcess( SHELLEXECUTEINFO exInfo, DWORD timeout_interval=0 )
+	{
+		//CloseHandle( pInfo.hThread );
+		WaitForInputIdle( exInfo.hProcess, INFINITE );
+
+		if( WaitForSingleObject( exInfo.hProcess, timeout_interval ) == WAIT_TIMEOUT )
+		{
+			EnumWindows( &SendWMCloseMsg, (LPARAM)exInfo.hwnd );
+			// force termination if app cannot be closed.
+			if( WaitForSingleObject( exInfo.hProcess, timeout_interval ) == WAIT_TIMEOUT )
+				TerminateProcess( exInfo.hProcess, 0 );
+		}
+
+		CloseHandle( exInfo.hProcess );
+	}
+
+
+}// end of OreOreLib
+
+
+#endif // !WIN_HELPER_FUNCTIONS_H
--- test/testSubprocess/testSubprocess_main.cpp
+++ test/testSubprocess/testSubprocess_main.cpp
@@ -14,6 +14,8 @@
 
 // https://stackoverflow.com/questions/33526534/run-python-script-from-c
 
+#include	<oreore/sys/win/SystemHelperFuncs.h>
+
 #include <windows.h>
 #include <iostream>
 using namespace std;
@@ -21,77 +23,78 @@ using namespace std;
 
 int main()
 {
+	//{
+	//	//=================== アプリケーション名で起動/終了をコントロールする場合 =================//
+
+	//	system( "start D:/ProgramData/Anaconda3/python.exe test.py" );//"start notepad.exe" );//
+	//	    
+	//	cout << "!!!!\n";
+	//	Sleep( 1000 );
+
+	//	system( "taskkill /F /IM notepad.exe >nul 2>&1" );
+	//}
+
 	{
-		////=================== アプリケーション名で起動/終了をコントロールする場合 =================//
+		//=================== プロセスハンドル使って起動/終了をコントロールする場合 ================//
 
-		//system( "start D:/ProgramData/Anaconda3/python.exe test.py" );//"start notepad.exe" );//
-		//    
-		//cout << "!!!!\n";
-		//Sleep( 1000 );
+		// ShellExecuteEx
+		SHELLEXECUTEINFO exInfo ={ 0 };
+		exInfo.cbSize = sizeof( SHELLEXECUTEINFO );
+		exInfo.fMask = SEE_MASK_NOCLOSEPROCESS;//SEE_MASK_DEFAULT;//
+		exInfo.lpVerb = L"open";
+		exInfo.lpFile = L"D:/ProgramData/Anaconda3/python.exe";//L"c:/windows/notepad.exe";
+		exInfo.lpParameters = L"test.py";
+		exInfo.nShow = SW_NORMAL;
 
-		//system( "taskkill /F /IM notepad.exe >nul 2>&1" );
+		ShellExecuteEx( &exInfo );// ShellExecute( 0, L"open", L"c:\\windows\\notepad.exe", 0, 0, SW_SHOW );
+		cout << exInfo.hProcess << endl;
+
+		Sleep( 5000 );
+
+		// これでプロセス生きてるかどうか分かる
+		cout << (WAIT_TIMEOUT==WaitForSingleObject( exInfo.hProcess, 0 )) << endl;
+
+		OreOreLib::CloseProcess( exInfo );
+		//TerminateProcess( exInfo.hProcess, 1 );
 	}
 
 	//{
-	//	//=================== プロセスハンドル使って起動/終了をコントロールする場合 ================//
+	//	//=================== CreateProcess/CloseHandle使って起動/終了をコントロールする場合 ================//
 
-	//	// ShellExecuteEx
-	//	SHELLEXECUTEINFO exInfo ={ 0 };
-	//	exInfo.cbSize = sizeof( SHELLEXECUTEINFO );
-	//	exInfo.fMask = SEE_MASK_NOCLOSEPROCESS;//SEE_MASK_DEFAULT;//
-	//	exInfo.lpVerb = L"open";
-	//	exInfo.lpFile = L"D:/ProgramData/Anaconda3/python.exe";//L"c:/windows/notepad.exe";
-	//	exInfo.lpParameters = L"test.py";
-	//	exInfo.nShow = SW_NORMAL;
+	//	PROCESS_INFORMATION pInfo = { 0 };
+	//	STARTUPINFO sInfo = { 0 };
+	//	sInfo.cb = sizeof( sInfo );
 
-	//	ShellExecuteEx( &exInfo );// ShellExecute( 0, L"open", L"c:\\windows\\notepad.exe", 0, 0, SW_SHOW );
-	//	cout << exInfo.hProcess << endl;
+	//	//wchar_t commandline[] = L"notepad.exe test.py";
+	//	wchar_t commandline[] = L"D:/ProgramData/Anaconda3/python.exe";
 
-	//	Sleep( 5000 );
+	//	auto ret = CreateProcess(
+	//		NULL,//L"c:\\Windows\\System32\\notepad.exe",
+	//		commandline,
+	//		NULL,
+	//		NULL,
+	//		FALSE,
+	//		0,
+	//		NULL,
+	//		NULL,
+	//		&sInfo,
+	//		&pInfo
+	//	);
 
-	//	// これでプロセス生きてるかどうか分かる
-	//	cout << (WAIT_TIMEOUT==WaitForSingleObject( exInfo.hProcess, 0 )) << endl;
+	//	if( ret == 0 )
+	//	{
+	//		cout << GetLastError() << endl;
+	//	}
+	//	else
+	//	{
+	//		Sleep( 5000 );
 
-	//	TerminateProcess( exInfo.hProcess, 1 );
+	//		//cout << ( WAIT_TIMEOUT==WaitForSingleObject( pInfo.hProcess, 0 ) ) << endl;
+	//		OreOreLib::CloseProcess( pInfo );
+	//		//TerminateProcess( pInfo.hProcess, 1 );
+	//	}
+
 	//}
-
-
-	{
-		//=================== CreateProcess/CloseHandle使って起動/終了をコントロールする場合 ================//
-
-		PROCESS_INFORMATION p = { 0 };
-		STARTUPINFO s = { 0 };
-		s.cb = sizeof( s );
-
-		wchar_t commandline[] = L"notepad.exe test.py";
-
-		auto ret = CreateProcess(
-			NULL,//L"c:\\Windows\\System32\\notepad.exe",
-			commandline,
-			NULL,
-			NULL,
-			FALSE,
-			0,
-			NULL,
-			NULL,
-			&s,
-			&p
-		);
-
-		if( ret == 0 )
-		{
-			cout << GetLastError() << endl;
-		}
-		else
-		{
-			Sleep( 5000 );
-
-			//cout << ( WAIT_TIMEOUT==WaitForSingleObject( p.hProcess, 0 ) ) << endl;
-
-			TerminateProcess( p.hProcess, 1 );
-		}
-
-	}
 
 
 	return 0;
